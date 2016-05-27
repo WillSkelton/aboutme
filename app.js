2dea0c991e2961b28c55ade36a12c3dc4949fc6a
Sugarblade <William.f.skelton@outlook.com>
2016-05-26 21:02:06 -0700

    Added statements to app.js to tell the user that they are correct

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ if (wantsToPlay === "yes") {
 	var userGuess = prompt("Poor people have it. Rich people need it. If you eat it you die. What is it? ").toLowerCase();
 	if(userGuess === "nothing"){
 		score++;
+		alert("Correct");
 	}
 
 	var userGuess = prompt("I'm tall when I'm young and I'm short when I'm old. What am I?").toLowerCase();
 	if (userGuess === "candle"){
-			score++;
+		alert("Correct");
+		score++;
 	}
 	var userGuess = prompt("Mary's father has 5 daughters - Nana, Nene, Nini, Nono. What is the fifth daughters name?").toLowerCase();
 	if (userGuess === "mary"){
+		alert("Correct");
 		score++;
 	}
 
