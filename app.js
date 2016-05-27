4687e045e89a4cb84e5a95cc0f54704c454dc527
Sugarblade <William.f.skelton@outlook.com>
2016-05-26 20:44:48 -0700

    finished guessing game and added resume portion to index .html

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
-while (true) {
-	var wantsToPlay = prompt("Would you like to play a game? (Yes/no)").toLowerCase();
-	if (wantsToPlay === "yes") {
-
-	} else if(wantsToPlay === "no") {
-
-		break;
-
-	}else {
-		alert('"' + wantsToPlay + '" is not yes or no.');
+var wantsToPlay = prompt("Would you like to play a game? (Yes/no)").toLowerCase();
+if (wantsToPlay === "yes") {
+	var score = 0;
+	var userGuess = prompt("Poor people have it. Rich people need it. If you eat it you die. What is it? ").toLowerCase();
+	if(userGuess === "nothing"){
+		score++;
+		var userGuess = prompt("I’m tall when I’m young and I’m short when I’m old. What am I?").toLowerCase();
+			if (userGuess === "candle"){
+				score++;
+				var userGuess = prompt("Mary’s father has 5 daughters – Nana, Nene, Nini, Nono. What is the fifth daughters name?").toLowerCase();
+						if (userGuess === "mary"){
+							score++;
+						}
+			}
+		alert("You guessed " + score + " out of 3 correct answers. Congradulations!")
 	}
 }
